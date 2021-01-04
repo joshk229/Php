c421e73105a921a878219b806bcfc87cd8af085a
josh <2135908@talnet.nl>
2021-01-04 12:49:34 +0100

    h3

---
+++ h3/index.php
@@ -0,0 +1,16 @@
+<?php?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Opdrachten</title>
+</head>
+<body>
+<?php
+for ($i = 1; $i<=9 ;$i++){
+    echo "<img src= 'apen/aap".$i.".jpg'>";
+}
+?>
+<img src='apen/aap2.jpg'>
+</body>
+</html>
